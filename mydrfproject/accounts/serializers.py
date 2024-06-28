5189f4f419aad117c9ae9de667cde2a0642c529c
Ashish Singh <ashishsingh2b@gmail.com>
2024-06-28 14:14:26 +0530

    currectionneed commit

---
+++ mydrfproject/accounts/serializers.py
@@ -0,0 +1,27 @@
+# accounts/serializers.py
+
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'confirm_password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')  # Remove confirm_password from the validated data
+        user = CustomUser(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
